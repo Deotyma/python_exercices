6204add8daa0d78b106f8e174fd7a96e45d7a403
Ewa Kadziolka <kalistaewa@gmail.com>
2022-07-05 07:13:12 +0200

    hangman game first steps

---
+++ hangman.py
@@ -0,0 +1,3 @@
+import random
+
+words = "shave pin committee protest mainstream heaven respect precedent failure smell opposed paper translate place crutch flash frog conference koran put ex frighten plane crude innocent doubt minor looting different testify elite tired officer blame objective swim acid book judicial professional stereotype ankle visual trance rib church ride facade cool recover"
