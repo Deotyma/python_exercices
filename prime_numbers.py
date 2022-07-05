a03b7d6e90bc9449fe12329e499eb21a04a0ae01
Ewa Kadziolka <kalistaewa@gmail.com>
2022-07-05 06:31:43 +0200

    prime numbers rectification

--- prime_numbers.py
+++ prime_numbers.py
@@ -2,15 +2,24 @@ def isPrimeNumber(number):
     if number > 1:
         for i in range(2,number):
             if number % i == 0:
-                print(f"{i} times {number//i} is {number}")
-                print(f"{number} is not a prime number")
-                break
+                #print(f"{i} times {number//i} is {number}")
+                #print(f"{number} is not a prime number")
+                return False    
+        return True
+    else:
+        return False    
 
-            else:
-                print(f"{number} is a prime number")
-                break
+print("7 is prime", isPrimeNumber(7))
+print("9 is prime", isPrimeNumber(9))
+#isPrimeNumber(56)
+#isPrimeNumber(6883)
+#isPrimeNumber(6882)
 
-isPrimeNumber(7)
-isPrimeNumber(56)
-isPrimeNumber(6883)
-isPrimeNumber(6882)
+def allPrimeNumbers(num):
+    for i in range(num):
+        if isPrimeNumber(i):
+            print(i, end = ", ")
+
+
+print("prime numbers to 100")
+allPrimeNumbers(100)
